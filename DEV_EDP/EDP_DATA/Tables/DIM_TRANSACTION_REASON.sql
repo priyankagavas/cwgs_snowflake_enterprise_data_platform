67b5c8424d9fd14c2346872ebef78a4e00349f97
Aniket Mitra <103688460+aniketmitra2022@users.noreply.github.com>
2022-04-21 17:14:16 +0530

    Delete DEV_EDP directory

--- DEV_EDP/EDP_DATA/Tables/DIM_TRANSACTION_REASON.sql
+++
@@ -1,9 +0,0 @@
-create or replace TABLE DEV_EDP.EDP_DATA.DIM_TRANSACTION_REASON (
-	REASON_ID NUMBER(38,0) NOT NULL autoincrement COMMENT 'System generated reason unique number',
-	REASON_CODE VARCHAR(30) COMMENT 'A code that identifies an order status reason for return, cancellation etc',
-	REASON_DESC VARCHAR(200) COMMENT 'Description of an order status reason for return, cancellation etc',
-	REASON_CATEGORY_DESC VARCHAR(200) COMMENT 'Description of an order reason category, such as return, cancellation etc',
-	RECORD_LOAD_DTM DATE DEFAULT CURRENT_DATE() COMMENT 'Record load datetime',
-	constraint XPKSTSREASONDIM primary key (REASON_ID)
-)COMMENT='List of order status reasons, such as return , cancelled order reason'
-;
--- DEV_EDP/EDP_DATA/Tables/DIM_TRANSACTION_STATUS.sql
+++
@@ -1,8 +0,0 @@
-create or replace TABLE DEV_EDP.EDP_DATA.DIM_TRANSACTION_STATUS (
-	TRANSACTION_STATUS_ID NUMBER(38,0) NOT NULL autoincrement COMMENT 'System generated unique transaction status identifier, such as order, cancelled, shipped, picked',
-	TRANSACTION_STATUS_CODE VARCHAR(30) COMMENT 'A code that identifies transaction status, such as order, cancelled, shipped, picked, booked, return, closed',
-	TRANSACTION_STATUS_DESC VARCHAR(80) COMMENT 'Description of transaction status, such as order, cancelled, shipped, picked,booked, return',
-	RECORD_LOAD_DTM DATE DEFAULT CURRENT_DATE() COMMENT 'Record load datetime',
-	constraint XPKTRNSSTSDIM primary key (TRANSACTION_STATUS_ID)
-)COMMENT='List of transaction status, such as order, cancelled, shipped, picked, booked'
-;
